807fa3fc7f481bf3fc94df609f73681f31fdc0f2
BaradlaiPatrik <baradlaipatrik008@gmail.com>
2026-01-12 17:59:52 +0100

    Frissítés

--- src/Model/Student.php
+++ src/Model/Student.php
@@ -1,33 +1,23 @@
 <?php
 declare(strict_types=1);
+
 namespace App\Model;
 
-class Student {
-    private string $firstName;
-    private string $lastName;
-    private string $gender;
-    private string $className;
-    private array $grades;
-
+class Student
+{
     public function __construct(
-        string $firstName,
-        string $lastName,
-        string $gender,
-        string $className,
-        array $grades
-    ) {
-        $this->firstName = $firstName;
-        $this->lastName = $lastName;
-        $this->gender = $gender;
-        $this->className = $className;
-        $this->grades = $grades;
-    }
+        private string $firstName,
+        private string $lastName,
+        private string $gender,
+        private string $className,
+        private array $grades = []
+    ) {}
 
-    public function getFullName(): string {
-        return $this->lastName . ' ' . $this->firstName;
-    }
+    public function getFullName(): string { return "$this->lastName $this->firstName"; }
 
-    public function getGrades(): array {
-        return $this->grades;
-    }
+    public function getGender(): string { return $this->gender; }
+
+    public function getClassName(): string { return $this->className; }
+
+    public function getGrades(): array { return $this->grades; }
 }
